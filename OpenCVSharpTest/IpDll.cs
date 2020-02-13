503f4e88da7bf25e672fd68882c361e797d44fe0
sim511777 <sim51177@gmail.com>
2020-02-13 21:48:52 +0900

    public static extern 순으로 변경

--- OpenCVSharpTest/IpDll.cs
+++ OpenCVSharpTest/IpDll.cs
@@ -8,16 +8,16 @@ using System.Runtime.InteropServices;
 namespace OpenCVSharpTest {
     class IpDll {
         [DllImport("IP.dll")] public static extern void InverseC(IntPtr buf, int bw, int bh, int stride);
-        [DllImport("IP.dll")] extern public static void InverseSse(IntPtr buf, int bw, int bh, int stride, bool useParallel);
-        [DllImport("IP.dll")] extern public static void InverseVec(IntPtr buf, int bw, int bh, int stride);
-        [DllImport("IP.dll")] extern public static void InverseAvx(IntPtr buf, int bw, int bh, int stride);
-        [DllImport("IP.dll")] extern public static void ErodeC(IntPtr srcBuf, IntPtr dstBuf, int bw, int bh, int step, bool useSse, bool useParallel);
-        [DllImport("IP.dll")] extern public static void ErodeIpp(IntPtr srcPtr, IntPtr dstPtr, int bw, int bh, int step);
-        [DllImport("IP.dll")] extern public static void DummyFunction(IntPtr srcPtr, IntPtr dstPtr, int bw, int bh, int step, int sleepMs);
-        [DllImport("IP.dll")] extern public static void ErodeIppRoi(IntPtr srcPtr, IntPtr dstPtr, int bw, int bh, int step, int roiX, int roiY, int roiW, int roiH);
-        [DllImport("IP.dll")] extern public static void GetString([MarshalAs(UnmanagedType.LPWStr)] StringBuilder sb);
-        [DllImport("IP.dll")] extern public static void SetString([MarshalAs(UnmanagedType.LPWStr)] string str);
-        [DllImport("IP.dll")] extern public static unsafe void Devernay(double** x, double** y, int* N, int** curve_limits, int* M, double* image, int X, int Y, double sigma, double th_h, double th_l);
-        [DllImport("IP.dll")] extern public static unsafe void FreeBuffer(void* buffer);
+        [DllImport("IP.dll")] public static extern void InverseSse(IntPtr buf, int bw, int bh, int stride, bool useParallel);
+        [DllImport("IP.dll")] public static extern void InverseVec(IntPtr buf, int bw, int bh, int stride);
+        [DllImport("IP.dll")] public static extern void InverseAvx(IntPtr buf, int bw, int bh, int stride);
+        [DllImport("IP.dll")] public static extern void ErodeC(IntPtr srcBuf, IntPtr dstBuf, int bw, int bh, int step, bool useSse, bool useParallel);
+        [DllImport("IP.dll")] public static extern void ErodeIpp(IntPtr srcPtr, IntPtr dstPtr, int bw, int bh, int step);
+        [DllImport("IP.dll")] public static extern void DummyFunction(IntPtr srcPtr, IntPtr dstPtr, int bw, int bh, int step, int sleepMs);
+        [DllImport("IP.dll")] public static extern void ErodeIppRoi(IntPtr srcPtr, IntPtr dstPtr, int bw, int bh, int step, int roiX, int roiY, int roiW, int roiH);
+        [DllImport("IP.dll")] public static extern void GetString([MarshalAs(UnmanagedType.LPWStr)] StringBuilder sb);
+        [DllImport("IP.dll")] public static extern void SetString([MarshalAs(UnmanagedType.LPWStr)] string str);
+        [DllImport("IP.dll")] public static extern unsafe void Devernay(double** x, double** y, int* N, int** curve_limits, int* M, double* image, int X, int Y, double sigma, double th_h, double th_l);
+        [DllImport("IP.dll")] public static extern unsafe void FreeBuffer(void* buffer);
     }
 }
